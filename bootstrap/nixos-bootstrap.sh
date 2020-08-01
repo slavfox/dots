51eff8c27fccf429eaca30a8865b5ee8cb0bf893
Slavfox <slavfoxman@gmail.com>
2020-08-01 16:32:48 +0200

    Don't add the non-root channel

--- bootstrap/nixos-bootstrap.sh
+++ bootstrap/nixos-bootstrap.sh
@@ -8,7 +8,6 @@ PS4="\n\033[1;33m::\033[0m "; set -x
 git clone https://github.com/slavfox/dots.git ~/dots
 sudo ln -s ~/dots/dotfiles/nixos /etc/nixos/cfg
 
-nix-channel --add https://nixos.org/channels/nixos-unstable nixos
 sudo nix-channel --add https://nixos.org/channels/nixos-unstable nixos
 
 sudo nixos-rebuild switch --upgrade
