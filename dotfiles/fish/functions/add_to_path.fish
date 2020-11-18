768b2c08ef3b5163f28c423170ac0aa64fd9b134
Slavfox <slavfoxman@gmail.com>
2020-11-18 08:07:15 +0100

    replace Gollum with Trilium

--- dotfiles/fish/functions/add_to_path.fish
+++ dotfiles/fish/functions/add_to_path.fish
@@ -1,6 +1,6 @@
 function add_to_path --description 'Add to user paths if not already added'
     for p in $argv
-        if test -d p
+        if test -d $p
             set abs (realpath $p)
             if not string match -q "*$abs*" $fish_user_paths
                 set -xU fish_user_paths $fish_user_paths $abs
--- dotfiles/fish/functions/fish_greeting.fish
+++ dotfiles/fish/functions/fish_greeting.fish
@@ -2,6 +2,7 @@ function fish_greeting
 if type -q pfetch
     pfetch
 else
+    sensors
     uptime
 end
 end
