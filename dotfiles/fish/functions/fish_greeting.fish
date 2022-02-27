78628ceaad192634005622ed46c215a6d8a2aa19
Slavfox <slavfoxman@gmail.com>
2022-02-27 04:05:04 +0100

    stuff

--- dotfiles/fish/functions/fish_greeting.fish
+++ dotfiles/fish/functions/fish_greeting.fish
@@ -2,7 +2,9 @@ function fish_greeting
 if type -q pfetch
     pfetch
 else
-    sensors
+    if type -q sensors
+        sensors
+    end
     uptime
 end
 end
--- dotfiles/fish/functions/git.fish
+++
@@ -1,3 +0,0 @@
-function git --wraps=hub --description 'alias git hub'
-  hub  $argv;
-end
